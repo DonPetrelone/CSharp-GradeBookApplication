01240f500627272228084a230be4512d75f04e41
Vinicius Petrelli Cicerone <vinicius.petrellicicerone@wmich.edu>
2020-05-25 11:32:56 -0400

    Completed module

---
+++ GradeBook/GradeBooks/StandardGradebook.cs
@@ -0,0 +1,13 @@
+using GradeBook.Enums;
+using GradeBook.GradeBooks;
+
+namespace GradeBooks.GradeBooks
+{
+    public class StandardGradebook : BaseGradeBook
+    {
+        public StandardGradebook(string name) : base(name)
+        {
+            
+        }
+    }
+}
