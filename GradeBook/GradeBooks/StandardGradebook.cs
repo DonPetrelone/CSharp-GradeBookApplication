d859ded1d90a5e394223d530542d7666e5d2d332
Vinicius Petrelli Cicerone <vinicius.petrellicicerone@wmich.edu>
2020-05-25 11:55:31 -0400

    Completed module

--- GradeBook/GradeBooks/StandardGradebook.cs
+++ GradeBook/GradeBooks/StandardGradebook.cs
@@ -3,7 +3,7 @@ using GradeBook.Enums;
 namespace GradeBook.GradeBooks
 {
     public class StandardGradeBook : BaseGradeBook
-    { 
+    {
         public StandardGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Standard;
